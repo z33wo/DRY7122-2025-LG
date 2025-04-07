a4d98ee9293003bfe66921d738b21ec2ca3fd94f
z33wo <ben.lizama@duocuc.cl>
2025-04-07 16:22:42 +0000

    también este

---
+++ script_python1.py
@@ -0,0 +1,11 @@
+print("Evaluación N°1 Programación y Redes Virtualizadas")
+
+cantidad = input("Ingrese la cantidad de integrantes del grupo: ")
+integrante1 = input("Ingrese el nombre del primer integrante del grupo: ")
+integrante2 = input("Ingrese el nombre del segundo integrante del grupo: ")
+
+print("El primer integrante de este grupo es:",(integrante1))
+print("El segundo integrante de este grupo es:",(integrante2))
+print("La cantidad de integrantes de este grupo son:",(cantidad))
+
+
