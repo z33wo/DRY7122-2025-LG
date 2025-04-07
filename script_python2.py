20594aaaa7729e8f6675c935dd5af1747deddae0
z33wo <ben.lizama@duocuc.cl>
2025-04-07 16:23:43 +0000

    agrego este igual pq quiero tener un 7

---
+++ script_python2.py
@@ -0,0 +1,14 @@
+print("Script 2 de Evaluación")
+
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu Apellido: ")
+sección = input("¿De qué sección eres?: ")
+sede = input("¿A qué sede correspondes?: ")
+
+print("Información del estudiante")
+print("El nombre del estudiante es",(nombre))
+print("El apellido del estudiante es:",(apellido))
+print("La sección del estudiante es:",(sección))
+print("La sede al que estudiante va es:",(sede))
+
+
